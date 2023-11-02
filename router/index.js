d5eb8db33182a82c886306eab2f6036d8eae660f
Muhammad Syahrul Romadhon <135720528+MuhSyahrul6@users.noreply.github.com>
2023-11-02 14:11:30 +0700

    Update Index.js

--- router/index.js
+++ router/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
+const routeMahasiswa = require("./mahasiswa");
+const routeDosen = require("./dosen");
+const routeMatKul = require("./matakuliah");
+const routeDsnMat = require("./dosenmatkul");
+const routeJadwalMat = require("./jadwalmatakuliah");
+const routeMahasiswaBimbingan = require("./mahasiswabimbingan");
 const route = express.Router()
 
-route.get('/',exampleController.index)
+route.get('/', exampleController.index)
+route.use('/mahasiswa', routeMahasiswa)
+route.use('/dosen', routeDosen)
+route.use('/matkul', routeMatKul)
+route.use('/dosen-matkul', routeDsnMat)
+route.use('/jadwal-matkul', routeJadwalMat)
+route.use('/mahasiswa-bimbingan', routeMahasiswaBimbingan)
 
 module.exports = route
